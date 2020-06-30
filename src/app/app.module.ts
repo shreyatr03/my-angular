61d7911a00db769728e5560e20e0ceed4c7e8f09
shreya <shreyatr@outlook.com>
2020-06-30 19:52:38 +0530

    header and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule
