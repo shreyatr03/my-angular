ce93a387387f4b2d4ea1153527a9af15e9a525ff
shreya <shreyatr@outlook.com>
2020-07-08 12:04:27 +0530

    trial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,9 @@ import { SketchesComponent } from './sketches/sketches.component';
 //This is my case
 const routes: Routes = [
     { path: '', component: AboutComponent},
-    { path: '\skills', component: SkillsComponent},
-    { path: '\projects', component: ProjectsComponent},
-    { path: '\sketches', component: SketchesComponent}
+    { path: 'skills', component: SkillsComponent},
+    { path: 'projects', component: ProjectsComponent},
+    { path: 'sketches', component: SketchesComponent}
 ];
 
 @NgModule({
